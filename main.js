e978ebfd71842698c5c972a7ca42973d41db358a
Jana Maskova <jana.maskova93@gmail.com>
2022-02-19 20:49:46 +0100

    zalozeni html, fce na prvni button

--- main.js
+++ main.js
@@ -1,1 +1,24 @@
-// nejaky text
+function prehrat(){
+    let audioFile = document.getElementById('zvuk');
+    audioFile.play();
+}
+
+function pauza(){
+    
+}
+
+function vypnout(){
+    
+}
+
+function hlasitostStredni(){
+    
+}
+
+function hlasitostMaximalni(){
+    
+}
+
+function spustitZnovu(){
+    
+}
